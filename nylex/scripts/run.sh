79549eedd11403989e61b87383e52d065e675935
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-09-19 14:41:03 -0700

    Found an update hosts script, so I'm trying it out.

--- nylex/scripts/run.sh
+++ nylex/scripts/run.sh
@@ -5,4 +5,5 @@ set -e
 service apache2 restart;
 service nagios start;
 service ssh start;
+# /scripts/docker-update-hosts.sh -a 127.0.0.1 -h /etc/hosts -q -v;
 tail -f /usr/local/nagios/var/nagios.log;
